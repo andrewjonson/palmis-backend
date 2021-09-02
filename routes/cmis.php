d1853ef39ea848116ab5ea5de84edc5a57903aba
fmtorino <66545772+fmtorino@users.noreply.github.com>
2021-09-02 10:58:24 +0800

    cmis route

--- routes/cmis.php
+++ routes/cmis.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/Resource.php';
 $router->group(['middleware' => ['jwt', 'verified', 'screenLockDisabled', 'modular:cmis'], 'prefix' => '/api/'.config('app.version').'/cmis'], function() use($router) {
     resource('/appurtenance', 'CmisService\References\AppurtenanceController', $router);
     resource('/subfactor/name', 'CmisService\References\SubFactorController', $router);
-    resource('/sublvl1', 'CmisService\References\SubLevelOneController', $router);
+    resource('/sublvl1/name', 'CmisService\References\SubLevelOneController', $router);
     resource('/purpose', 'CmisService\References\PurposeController', $router);
     resource('/award', 'CmisService\References\AwardController', $router);
     resource('/award/type', 'CmisService\References\AwardController', $router);
