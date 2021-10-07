bf8df35d352099c7a4965b7b0d2786ce2ac3bd50
Andrew Jonson <andrew_jonson09@yahoo.com>
2021-10-07 12:26:48 +0800

    Modified config services

--- config/services.php
+++ config/services.php
@@ -4,5 +4,6 @@ return [
     'mpf_base_uri' => env('MPF_BASE_URI'),
     'mpis_base_uri' => env('MPIS_BASE_URI'),
     'orderpub_base_uri' => env('ORDERPUB_BASE_URI'),
-    'cmis_base_uri' => env('CMIS_BASE_URI')
+    'cmis_base_uri' => env('CMIS_BASE_URI'),
+    'mpf_secret' => env('MPF_SECRET')
 ];
