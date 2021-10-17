a9d7171f588c6ab5c2e44008f676d344ace9b82a
fmtorino <66545772+fmtorino@users.noreply.github.com>
2021-10-17 14:31:11 +0800

    fix route

--- routes/toeis.php
+++ routes/toeis.php
@@ -3,5 +3,5 @@
 require_once __DIR__ . '/Resource.php';
 
 $router->group(['middleware' => ['auth:api', 'verified', 'screenLockDisabled', 'modular:toeis'], 'prefix' => '/api/'.config('app.version').'/toeis'], function() use($router) {
-    resource('/echelons', 'CmisService\References\EchelonController', $router);
+    resource('/echelons', 'ToeisService\References\EchelonController', $router);
 });
