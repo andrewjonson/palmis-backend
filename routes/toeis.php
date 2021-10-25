2ffa8d743bfdbbc75eee9632fd6c61814765a917
fmtorino <66545772+fmtorino@users.noreply.github.com>
2021-10-25 19:38:18 +0800

    fix toeis routes

--- routes/toeis.php
+++ routes/toeis.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/Resource.php';
 $router->group(['middleware' => ['auth:api', 'verified', 'screenLockDisabled', 'modular:toeis'], 'prefix' => '/api/'.config('app.version').'/toeis'], function() use($router) {
     resource('/echelons', 'ToeisService\References\EchelonController', $router);
 
-    $router->post('/unit-all-active', 'ApiService\v1\ToeisService\Transactions\UnitController@index');
-    $router->post('/unit-per-id/{id}', 'ApiService\v1\ToeisService\Transactions\UnitController@getUnitById');
-    $router->post('/unit-concat/{id}', 'ApiService\v1\ToeisService\Transactions\UnitController@getUnitConcatById');
+    $router->get('/unit-all-active', 'ApiService\v1\ToeisService\Transactions\UnitController@index');
+    $router->get('/unit-per-id/{id}', 'ApiService\v1\ToeisService\Transactions\UnitController@getUnitById');
+    $router->get('/unit-concat/{id}', 'ApiService\v1\ToeisService\Transactions\UnitController@getUnitConcatById');
 });
