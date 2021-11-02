001065d3fcb60f4c9e2d37af611aa8223f0d36ff
fmtorino <66545772+fmtorino@users.noreply.github.com>
2021-11-02 13:15:14 +0800

    fix personnel unit

--- app/Services/ApiService/v1/MpisService/Transactions/PersonnelUnit.php
+++ app/Services/ApiService/v1/MpisService/Transactions/PersonnelUnit.php
@@ -17,6 +17,6 @@ class PersonnelUnit
 
     public function createPersonnelUnit($data)
     {
-        return $this->performRequest('POST', '/mpis/store-personnel-duty-assignment', $data);
+        return $this->performRequest('POST', '/mpis/store-personnel-unit-assignment', $data);
     }
 }
